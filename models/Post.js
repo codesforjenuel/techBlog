582a557e03c767fd401ddaad2e936d99e45df008
codesforjenuel <codesforjenuel@gmail.com>
2021-11-13 08:37:50 -0800

    first update

---
+++ models/Post.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {
+    
+  }
+
+
+  Post.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: DataTypes.STRING
+        
+      },
+      content: {
+        type: DataTypes.STRING,
+       
+      },
+     
+      
+    },
+    {
+     
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'post',
+    }
+  );
+  
+  module.exports = Post;
